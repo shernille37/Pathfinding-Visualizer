9bfb2f3dafeffe5589f67324c41c3cc4cf701754
Shernille Licud <shernillelicud@Shernilles-MBP.Home>
2023-11-08 11:01:05 +0100

    Add grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import Node from './components/Node';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const NUM_ROWS = 20;
+  const NUM_COLS = 50;
+
+  const [nodes, setNodes] = useState([]);
+
+  useEffect(() => {
+    let temp_nodes = [];
+    for (let i = 0; i < NUM_ROWS; i++) {
+      let currentRow = [];
+
+      for (let j = 0; j < NUM_COLS; j++) {
+        currentRow.push([]);
+      }
+      temp_nodes.push(currentRow);
+    }
+
+    setNodes(temp_nodes);
+  }, []);
+  console.log(nodes);
+
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <div className='grid'>
+        {nodes.map((row, index1) => {
+          return (
+            <div key={index1} className='row'>
+              {row.map((node, index2) => (
+                <Node key={index2}></Node>
+              ))}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/components/Node.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/node.css';
+
+const Node = () => {
+  return <div className='node'></div>;
+};
+
+export default Node;
